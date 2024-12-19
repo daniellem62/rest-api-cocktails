73690590ade3a09abc1a29eeecca4a9edfb931ad
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2024-12-19 18:28:19 +0000

    Added patch and delete functions and listeners

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 3002;
 import { getCocktails,
   getCocktailById,
   addCocktail,
+  editCocktail,
+  deleteCocktail
  } from "./cocktail.js";
 
 app.use(express.json());
@@ -26,13 +28,34 @@ app.get("/cocktails/:id", async (req, res) => {
 })
 
 //MVP 3 - create a post listener that create a new cocktail
-// create a variable that gets the new cocktails for the request body
+// create a variable that gets the new cocktails from the request body
 // use the add cocktail function
 // send response
 app.post("/cocktails", async (req, res) => {
   const { id, name, alcoholic, category, glassType } = req.body;
   const cocktail = await addCocktail(id, name, alcoholic, category, glassType);
   res.json(cocktail);
+})
+
+//MVP 4 - create a patch listener that edits a cocktail
+// create a variable that gets the updated cocktail from the request body
+// use the edit cocktail function
+// send new cocktail
+app.patch("/cocktails/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, alcoholic, category, glassType } = req.body;
+  const newCocktail = await editCocktail(id, name, alcoholic, category, glassType);
+  res.json(newCocktail);
+})
+
+//MVP 5 - create a delete listener to delete cocktail
+// get the cocktail from the id
+// delete the cocktail
+// send deleted cocktail
+app.delete("/cocktails/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedCocktail = await deleteCocktail(id);
+  res.send(deletedCocktail)
 })
 
 app.listen(port, function () {
--- cocktail.js
+++ cocktail.js
@@ -44,3 +44,61 @@ export async function addCocktail(id, name, alcoholic, category, glassType) {
     return newCocktail;
 }
 
+//MVP 4
+// create a function to update a cocktail recipe
+// read all the cocktails
+// grab cocktail by id
+// write updated cocktails back to file
+export async function editCocktail(id, newName, newAlcoholic, newCategory, newGlassType, ) {
+  const cocktails = await readCocktails();
+
+  const cocktailToEdit = cocktails.find((cocktail) => cocktail.id === id);
+
+  if (cocktailToEdit) {
+    cocktailToEdit.name = newName ?? cocktailToEdit.name;
+    cocktailToEdit.alcoholic = newAlcoholic ?? cocktailToEdit.alcoholic;
+    cocktailToEdit.category = newCategory ?? cocktailToEdit.category;
+    cocktailToEdit.glassType = newGlassType ?? cocktailToEdit.glassType;
+  }
+
+  await writeCocktails(cocktails);
+  return cocktailToEdit;
+}
+//MVP 5
+// create a function to delete a cocktail recipe
+// read all the cocktails
+// grab cocktail by id
+// delete cocktail
+// write back to file
+// return deleted
+//export async function deleteCocktail(id) {
+//  let cocktails = await readCocktails();
+//  const cocktailToDelete = cocktails.find((cocktail) => cocktail.id === id);
+//  const indexToDelete = cocktails.findIndex((cocktail) => cocktail.id === id);
+//  if (cocktailToDelete) {
+//    const deletedCocktail = cocktails.splice(indexToDelete, 1)[0];
+//    await writeCocktails(cocktails);
+//    return deletedCocktail;
+//} 
+//}
+export async function deleteCocktail(id) {
+  // Read the list of cocktails
+  let cocktails = await readCocktails();
+  
+  // Find the index of the cocktail to delete
+  const indexToDelete = cocktails.findIndex((cocktail) => cocktail.id === id);
+
+  if (indexToDelete !== -1) {
+    // Remove the cocktail from the array using splice
+    const deletedCocktail = cocktails.splice(indexToDelete, 1)[0];
+
+    // Save the updated cocktails list back to storage
+    await writeCocktails(cocktails);
+
+    // Return the deleted cocktail
+    return deletedCocktail;
+  } else {
+    // If the cocktail was not found, return null or handle error
+    return null;  // or throw new Error('Cocktail not found');
+  }
+}
